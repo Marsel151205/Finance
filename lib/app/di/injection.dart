990606032badd0d535cb6b1f978e6b64da7b04da
MAMambetaliev <mamambetaliev@beeline.kg>
2026-03-15 19:31:40 +0600

    Remove add_entry bloc, state, and event, use case and add inject database, expense

--- lib/app/di/injection.dart
+++ lib/app/di/injection.dart
@@ -1,11 +1,25 @@
+import 'package:finance_tracker/app/database/database.dart';
+import 'package:finance_tracker/features/add_entry/data/repositories/expense_repository_impl.dart';
+import 'package:finance_tracker/features/add_entry/domain/use_cases/add_expense_use_case.dart';
 import 'package:get_it/get_it.dart';
 
-import '../../features/add_entry/domain/use_cases/add_entry_use_case.dart';
-import '../../features/add_entry/presentation/bloc/add_entry_bloc.dart';
+import '../../features/add_entry/domain/repositories/add_expense_repository.dart';
+import '../../features/add_entry/presentation/bloc/expense/expense_bloc.dart';
 
 final serviceLocator = GetIt.instance;
 
 void initDependencies() {
-  serviceLocator.registerFactory<AddEntryUseCase>(() => AddEntryUseCase());
-  serviceLocator.registerFactory<AddEntryBloc>(() => AddEntryBloc(serviceLocator()));
+  // Database
+  serviceLocator.registerSingleton<AppDatabase>(AppDatabase());
+
+  // Expense
+  serviceLocator.registerLazySingleton<ExpenseRepository>(
+    () => ExpenseRepositoryImpl(serviceLocator<AppDatabase>()),
+  );
+  serviceLocator.registerLazySingleton<AddExpenseUseCase>(
+    () => AddExpenseUseCase(serviceLocator<ExpenseRepository>()),
+  );
+  serviceLocator.registerFactory<ExpenseBloc>(
+    () => ExpenseBloc(serviceLocator()),
+  );
 }
