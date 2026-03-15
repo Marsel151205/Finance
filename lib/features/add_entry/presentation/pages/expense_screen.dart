01756a4aa305a39e95046f7cf6c3f713457ca2f6
MAMambetaliev <mamambetaliev@beeline.kg>
2026-03-16 00:50:15 +0600

    added bloc consumer

--- lib/features/add_entry/presentation/pages/expense_screen.dart
+++ lib/features/add_entry/presentation/pages/expense_screen.dart
@@ -2,40 +2,52 @@ import 'package:finance_tracker/core/themes/colors.dart';
 import 'package:finance_tracker/core/themes/dimens.dart';
 import 'package:finance_tracker/core/widgets/amount_input_field.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class ExpenseScreen extends StatelessWidget {
+class ExpenseScreen extends StatefulWidget {
   const ExpenseScreen({super.key});
 
   @override
+  State<StatefulWidget> createState() => _ExpenseScreenState();
+}
+
+class _ExpenseScreenState extends State<ExpenseScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(left: paddingLeft10, right: paddingRight10),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(height: 12),
-          Text(
-            'Сумма',
-            style: TextStyle(
-              color: secondaryTextColor,
-              fontSize: textSize14,
-              fontWeight: FontWeight.w400,
-            ),
-          ),
-          AmountInputField(),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              'Категории',
-              style: TextStyle(
-                color: secondaryTextColor,
-                fontSize: textSize14,
-                fontWeight: FontWeight.w400,
+    return BlocConsumer(
+      builder: (context, state) {
+        return Padding(
+          padding: EdgeInsets.only(left: paddingLeft10, right: paddingRight10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 12),
+              Text(
+                'Сумма',
+                style: TextStyle(
+                  color: secondaryTextColor,
+                  fontSize: textSize14,
+                  fontWeight: FontWeight.w400,
+                ),
               ),
-            ),
-          ), 
-        ],
-      ),
+              AmountInputField(),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Категории',
+                  style: TextStyle(
+                    color: secondaryTextColor,
+                    fontSize: textSize14,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+              
+            ],
+          ),
+        );
+      },
+      listener: (context, state) {},
     );
   }
 }
