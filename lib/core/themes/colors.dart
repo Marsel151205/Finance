e80ec321373bf74cadd28617deb2560a7e1d4c8b
MAMambetaliev <mamambetaliev@beeline.kg>
2026-03-15 19:36:08 +0600

    add new colors and sizes

--- lib/core/themes/colors.dart
+++ lib/core/themes/colors.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
 // Background Colors
-final Color backgroundColor = Color(0xFF0c061f);
+final Color backgroundColor = Color(0xFF131324);
 final Color backgroundGreen = Color(0xFF4dff4d);
 final Color backgroundRed = Color(0xFFfc3f3f);
 
@@ -9,3 +9,7 @@ final Color backgroundRed = Color(0xFFfc3f3f);
 final Color textColorPrimary = Color(0xFFffffff);
 final Color selectedTextColor = Color(0xFFffffff);
 final Color unselectedTextColor = Color(0xFF89888a);
+final Color secondaryTextColor = Color(0xFF666669);
+
+// Widget Colors
+final Color widgetColorPrimary = Color(0xFF5856D6);
--- lib/core/themes/dimens.dart
+++ lib/core/themes/dimens.dart
@@ -1,6 +1,29 @@
 // Paddings
 final double padding12 = 12;
+final double paddingVertical24 = 24;
+final double paddingVertical40 = 40;
+final double paddingHorizontal24 = 24;
+final double paddingLeft6 = 6;
+final double paddingRight6 = 6;
+final double paddingLeft10 = 10;
+final double paddingRight10 = 10;
+final double paddingTop10 = 10;
 
 // Text Sizes
+final double textSize30 = 30;
+final double textSize24 = 24;
 final double textSize20 = 20;
 final double textSize16 = 16;
+final double textSize14 = 14;
+
+// Heights
+final double height6 = 6;
+final double height12 = 12;
+final double height4 = 4;
+
+// Widths
+final double width120 = 120;
+final double width100 = 100;
+
+// Circulars
+final double circular2 = 2;
