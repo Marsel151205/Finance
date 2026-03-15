28aadc2517786db016da556314fdc6b85381bbf1
MAMambetaliev <mamambetaliev@beeline.kg>
2026-03-15 19:40:51 +0600

    Implement expense/income amount input UI

---
+++ lib/core/widgets/amount_input_field.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../themes/colors.dart';
+import '../themes/dimens.dart';
+
+class AmountInputField extends StatefulWidget {
+  const AmountInputField({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _AmountInputFieldState();
+}
+
+class _AmountInputFieldState extends State<AmountInputField> {
+  final TextEditingController _controller = TextEditingController(text: '');
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: backgroundColor,
+      padding: EdgeInsets.symmetric(
+        vertical: paddingVertical24,
+        horizontal: paddingHorizontal24,
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          IntrinsicWidth(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                IntrinsicWidth(
+                  child: TextField(
+                    controller: _controller,
+                    keyboardType: TextInputType.number,
+                    onTapOutside: (_) {},
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                    style: TextStyle(
+                      color: textColorPrimary,
+                      fontSize: textSize30,
+                      fontWeight: FontWeight.w500,
+                    ),
+                    decoration: InputDecoration(
+                      hintText: '0',
+                      hintStyle: TextStyle(color: textColorPrimary),
+                      border: InputBorder.none,
+                      isDense: true,
+                      contentPadding: EdgeInsets.zero,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                    left: paddingLeft6,
+                    top: paddingTop10,
+                  ),
+                  child: Text(
+                    '₽',
+                    style: TextStyle(
+                      color: secondaryTextColor,
+                      fontSize: textSize20,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: height6),
+          Container(
+            width: width100,
+            height: height4,
+            decoration: BoxDecoration(
+              color: widgetColorPrimary,
+              borderRadius: BorderRadius.circular(circular2),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
