23de569296120a492aaaae9b63c0813411f1e448
Marsel <mambetaliev.marsel@icloud.com>
2026-03-16 18:19:01 +0600

    Implement mock categories

--- lib/features/add_entry/domain/use_cases/get_expense_categories_use_case.dart
+++ lib/features/add_entry/domain/use_cases/get_expense_categories_use_case.dart
@@ -1,7 +1,18 @@
 import '../entities/expense_category_entity.dart';
 
-// class GetExpenseCategoriesUseCase {
-//   Future<List<ExpenseCategoryEntity>> getExpenseCategories() async {
-//
-//   }
-// }
+class GetExpenseCategoriesUseCase {
+  List<ExpenseCategoryEntity> getExpenseCategories() {
+    List<ExpenseCategoryEntity> expenseList = [
+      ExpenseCategoryEntity(title: 'Еда'),
+      ExpenseCategoryEntity(title: 'Транспорт'),
+      ExpenseCategoryEntity(title: 'Развелечение'),
+      ExpenseCategoryEntity(title: 'Жилье'),
+      ExpenseCategoryEntity(title: 'Здоровье'),
+      ExpenseCategoryEntity(title: 'Одежда'),
+      ExpenseCategoryEntity(title: 'Подписки'),
+      ExpenseCategoryEntity(title: 'Другое'),
+    ];
+
+    return expenseList;
+  }
+}
