94fc4d8522cd51134d83f2c71f6fdea07266d945
Marsel <mambetaliev.marsel@icloud.com>
2026-03-17 17:01:31 +0600

    optimize screen

--- lib/features/add_entry/presentation/pages/add_entry_page.dart
+++ lib/features/add_entry/presentation/pages/add_entry_page.dart
@@ -35,6 +35,7 @@ class _AddEntryPageState extends State<AddEntryPage>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       backgroundColor: backgroundColor,
       appBar: StandardAppBar(title: 'Новая запись'),
       body: Column(
