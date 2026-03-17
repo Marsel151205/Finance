2f5193380009420f848c8e27f2de77374b7964bb
Marsel <mambetaliev.marsel@icloud.com>
2026-03-17 17:02:20 +0600

    change location entities

--- lib/features/add_entry/data/repositories/expense_repository_impl.dart
+++ lib/features/add_entry/data/repositories/expense_repository_impl.dart
@@ -1,6 +1,6 @@
 import 'package:drift/drift.dart';
 import 'package:finance_tracker/features/add_entry/data/models/expense_data_extensions.dart';
-import 'package:finance_tracker/features/add_entry/domain/entities/expense_entity.dart';
+import 'package:finance_tracker/features/add_entry/domain/entities/expense/expense_entity.dart';
 import 'package:finance_tracker/features/add_entry/domain/repositories/add_expense_repository.dart';
 import 'package:fpdart/fpdart.dart';
 
--- lib/features/add_entry/domain/repositories/add_expense_repository.dart
+++ lib/features/add_entry/domain/repositories/add_expense_repository.dart
@@ -1,4 +1,4 @@
-import 'package:finance_tracker/features/add_entry/domain/entities/expense_entity.dart';
+import 'package:finance_tracker/features/add_entry/domain/entities/expense/expense_entity.dart';
 import 'package:fpdart/fpdart.dart';
 
 abstract class ExpenseRepository {
