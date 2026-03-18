c8f1a77753895661b83a28480b5d84f2c84ba41e
MAMambetaliev <mamambetaliev@beeline.kg>
2026-03-18 23:15:04 +0600

    sorted and added sizes

--- lib/core/themes/dimens.dart
+++ lib/core/themes/dimens.dart
@@ -4,10 +4,12 @@ final double paddingVertical24 = 24;
 final double paddingVertical40 = 40;
 final double paddingHorizontal24 = 24;
 final double paddingLeft6 = 6;
-final double paddingRight6 = 6;
 final double paddingLeft10 = 10;
+final double paddingRight6 = 6;
 final double paddingRight10 = 10;
 final double paddingTop10 = 10;
+final double paddingTop16 = 16;
+final double paddingBottom16 = 16;
 
 // Text Sizes
 final double textSize30 = 30;
@@ -21,11 +23,13 @@ final double textSize10 = 10;
 final double textSize8 = 8;
 
 // Heights
+final double height4 = 4;
 final double height6 = 6;
 final double height12 = 12;
-final double height4 = 4;
+final double height16 = 16;
 
 // Widths
+final double width12 = 12;
 final double width120 = 120;
 final double width100 = 100;
 
