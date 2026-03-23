b2b88bc9d80d03e84d593493af2dd1a801e8c810
Marsel <mambetaliev.marsel@icloud.com>
2026-03-23 11:02:36 +0600

    implementation bottom navigation bar

--- lib/app/router/routing.dart
+++ lib/app/router/routing.dart
@@ -1,7 +1,32 @@
+import 'package:finance_tracker/app/app_scaffold.dart';
 import 'package:finance_tracker/features/add_entry/presentation/pages/add_entry_page.dart';
+import 'package:finance_tracker/features/main/presentation/pages/main_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:go_router/go_router.dart';
 
+final _rootNavigatorKey = GlobalKey<NavigatorState>();
+final _shellNavigatorKey = GlobalKey<NavigatorState>();
+
 final goRouter = GoRouter(
-  initialLocation: '/',
-  routes: [GoRoute(path: '/', builder: (context, state) => AddEntryPage())],
+  navigatorKey: _rootNavigatorKey,
+  initialLocation: '/main',
+  routes: [
+    ShellRoute(
+      navigatorKey: _shellNavigatorKey,
+      builder: (context, state, child) {
+        final screenTitles = {'/main': 'Главная', '/add_entry': 'Новая запись'};
+        return AppScaffold(
+          screenTitle: screenTitles[state.uri.toString()] ?? 'йцу',
+          child: child,
+        );
+      },
+      routes: <RouteBase>[
+        GoRoute(path: '/main', builder: (context, state) => MainPage()),
+        GoRoute(
+          path: '/add_entry',
+          builder: (context, state) => AddEntryPage(),
+        ),
+      ],
+    ),
+  ],
 );
