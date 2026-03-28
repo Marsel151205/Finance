58ac85a0b0b23ab4724d60410f86e7b920392775
MAMambetaliev <mamambetaliev@beeline.kg>
2026-03-28 17:14:57 +0600

    Created a revenue list screen

---
+++ lib/features/main/presentation/pages/income_list_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class IncomeListScreen extends StatelessWidget {
+  const IncomeListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Income List');
+  }
+}
