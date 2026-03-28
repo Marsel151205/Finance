3163f92fbf61808711801ab6de1c55c7072af623
MAMambetaliev <mamambetaliev@beeline.kg>
2026-03-28 16:51:11 +0600

    change location

--- lib/shared/data/models/expense_data_extensions.dart
+++ lib/shared/data/models/expense_data_extensions.dart
@@ -1,6 +1,6 @@
-import 'package:finance_tracker/features/add_entry/domain/entities/expense/expense_entity.dart';
+import 'package:finance_tracker/shared/domain/entities/expense_entity.dart';
 
-import '../../../../app/database/database.dart';
+import '../../../app/database/database.dart';
 
 extension ExpenseDataMapper on ExpenseItemData {
   ExpenseEntity toEntity() {
