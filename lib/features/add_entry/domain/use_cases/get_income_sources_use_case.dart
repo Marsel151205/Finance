59faf9772da7937e8c8298040c61447dcc7416e4
Marsel <mambetaliev.marsel@icloud.com>
2026-03-31 17:34:23 +0600

    created a use_case that will return a list of income sources

---
+++ lib/features/add_entry/domain/use_cases/get_income_sources_use_case.dart
@@ -0,0 +1,13 @@
+class GetIncomeSourcesUseCase {
+  List<String> getIncomeSources() {
+    return [
+      'Зарплата',
+      'Родители',
+      'Друзья',
+      'Долг',
+      'Фриланс',
+      'Подарок',
+      'Другое',
+    ];
+  }
+}
