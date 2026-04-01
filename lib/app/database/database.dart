ebc139557fc1611b754bd56db2294c9135fcba60
Marsel <mambetaliev.marsel@icloud.com>
2026-04-01 16:15:00 +0600

    Created a new table in the database to save revenue

--- lib/app/database/database.dart
+++ lib/app/database/database.dart
@@ -1,5 +1,6 @@
 import 'package:drift/drift.dart';
 import 'package:drift_flutter/drift_flutter.dart';
+import 'package:finance_tracker/features/add_entry/presentation/widgets/income_sources.dart';
 import 'package:path_provider/path_provider.dart';
 
 part 'database.g.dart';
@@ -17,7 +18,19 @@ class ExpenseItem extends Table {
   TextColumn get comment => text().nullable()();
 }
 
-@DriftDatabase(tables: [ExpenseItem])
+class IncomeItem extends Table {
+  IntColumn get id => integer().autoIncrement()();
+
+  IntColumn get sum => integer()();
+
+  TextColumn get category => text().nullable()();
+
+  TextColumn get incomeSource => text().nullable()();
+
+  TextColumn get comment => text().nullable()();
+}
+
+@DriftDatabase(tables: [ExpenseItem, IncomeSources])
 class AppDatabase extends _$AppDatabase {
   AppDatabase([QueryExecutor? executor]) : super(executor ?? _openConnection());
 
