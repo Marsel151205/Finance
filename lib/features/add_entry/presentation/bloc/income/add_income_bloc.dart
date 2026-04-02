8b2bd3a5fb43850d5c2285605d0223a210e4942a
Marsel <mambetaliev.marsel@icloud.com>
2026-04-02 15:44:33 +0600

    Added a use case to the constructor to save income, added a field to store the index of the selected source of income. Implemented a database request to save income

--- lib/features/add_entry/presentation/bloc/income/add_income_bloc.dart
+++ lib/features/add_entry/presentation/bloc/income/add_income_bloc.dart
@@ -1,7 +1,9 @@
 import 'package:finance_tracker/features/add_entry/domain/use_cases/get_income_sources_use_case.dart';
+import 'package:finance_tracker/shared/presentation/models/income_model.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../domain/entities/expense_category_entity.dart';
+import '../../../domain/use_cases/add_income_use_case.dart';
 import '../../../domain/use_cases/get_expense_categories_use_case.dart';
 import 'add_income_event.dart';
 import 'add_income_state.dart';
@@ -9,22 +11,50 @@ import 'add_income_state.dart';
 class AddIncomeBloc extends Bloc<AddIncomeEvent, AddIncomeState> {
   final GetIncomeSourcesUseCase _getIncomeSourcesUseCase;
   final GetExpenseCategoriesUseCase _getExpenseCategoriesUseCase;
+  final AddIncomeUseCase _addIncomeUseCase;
 
   AddIncomeBloc(
     this._getIncomeSourcesUseCase,
     this._getExpenseCategoriesUseCase,
+    this._addIncomeUseCase,
   ) : super(AddIncomeState()) {
-    on<SaveIncomeEvent>((event, emit) {
-      emit(LoadingAddIncomeState());
+    on<SaveIncomeEvent>((event, emit) async {
+      await _saveIncome(event, emit);
     });
   }
 
   int? _selectedCategoryIndex;
+  int? _selectedIncomeSourceIndex;
   int _sum = 0;
-  String _selectedCategory = 'Без категории';
-  String _comment = 'Без комментария';
+  String? _selectedCategory;
+  String? _selectedIncomeSource;
+  String? _comment;
 
-  List<ExpenseCategoryEntity> getCategoriesList() =>
+  Future<void> _saveIncome(
+    SaveIncomeEvent event,
+    Emitter<AddIncomeState> emit,
+  ) async {
+    emit(LoadingAddIncomeState());
+    await Future.delayed(Duration(milliseconds: 500), () async {
+      final incomeModel = IncomeModelUi(
+        sum: _sum,
+        category: _selectedCategory ?? 'Другое',
+        incomeSource: _selectedIncomeSource ?? 'Другое',
+        comment: _comment ?? 'Без комментария',
+      ).toEntity();
+      final result = await _addIncomeUseCase.call(incomeModel);
+      result.fold(
+        (failure) {
+          emit(ErrorAddIncomeState(failure));
+        },
+        (success) {
+          emit(SuccessAddIncomeState());
+        },
+      );
+    });
+  }
+
+  List<CategoryEntity> getCategoriesList() =>
       _getExpenseCategoriesUseCase.getExpenseCategories();
 
   List<String> getIncomeSourcesList() =>
@@ -48,5 +78,15 @@ class AddIncomeBloc extends Bloc<AddIncomeEvent, AddIncomeState> {
 
   int? getSelectedCategoryIndex() {
     return _selectedCategoryIndex;
+  }
+
+  void setSelectedIncomeSourceIndex(int? index) {
+    _selectedIncomeSourceIndex = index;
+  }
+
+  int? getSelectedIncomeSourceIndex() => _selectedIncomeSourceIndex;
+
+  void setSelectedIncomeSource(String? incomeSource) {
+    _selectedIncomeSource = incomeSource;
   }
 }
