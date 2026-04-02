6c68e79dc9b58970e51fc1bf70b216cf4f0ec11a
Marsel <mambetaliev.marsel@icloud.com>
2026-04-02 19:18:41 +0600

    implement view income list

--- lib/features/main/presentation/pages/income_list_screen.dart
+++ lib/features/main/presentation/pages/income_list_screen.dart
@@ -1,10 +1,53 @@
+import 'package:finance_tracker/core/utils/loading_overlay.dart';
+import 'package:finance_tracker/core/utils/message_snack_bar.dart';
+import 'package:finance_tracker/features/main/presentation/bloc/income/income_list_state.dart';
+import 'package:finance_tracker/features/main/presentation/widgets/movement.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../bloc/income/income_list_bloc.dart';
 
 class IncomeListScreen extends StatelessWidget {
   const IncomeListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text('Income List');
+    return BlocConsumer<IncomeListBloc, IncomeListState>(
+      builder: (context, state) {
+        if (state is SuccessIncomeListState) {
+          LoadingOverlay.hide();
+          final incomeList = state.incomeList;
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  itemCount: incomeList.length,
+                  itemBuilder: (context, index) {
+                    final incomeItem = incomeList[index];
+                    return Padding(
+                      padding: EdgeInsets.all(8),
+                      child: Movement(incomeModel: incomeItem),
+                    );
+                  },
+                ),
+              ),
+            ],
+          );
+        }
+        return SizedBox();
+      },
+      listener: (context, state) {
+        if (state is LoadingIncomeListState) LoadingOverlay.show(context);
+        if (state is ErrorIncomeListState) {
+          LoadingOverlay.hide();
+          showMessageSnackBar(
+            context,
+            title: state.errorMessage,
+            status: false,
+          );
+        }
+      },
+    );
   }
 }
