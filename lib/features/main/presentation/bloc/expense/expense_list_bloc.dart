8fa480e44c96e1562b44fcd3f35b25a41d356170
Marsel <mambetaliev.marsel@icloud.com>
2026-04-02 19:17:21 +0600

    refactoring code

--- lib/features/main/presentation/bloc/expense/expense_list_bloc.dart
+++ lib/features/main/presentation/bloc/expense/expense_list_bloc.dart
@@ -1,16 +1,17 @@
-import 'package:finance_tracker/features/main/presentation/bloc/expense_list_state.dart';
 import 'package:finance_tracker/shared/presentation/models/expense_model.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../../domain/use_cases/get_expense_list_use_case.dart';
+import '../../../domain/use_cases/get_expense_list_use_case.dart';
 import 'expense_list_event.dart';
+import 'expense_list_state.dart';
 
 class ExpenseListBloc extends Bloc<ExpenseListEvent, ExpenseListState> {
   final GetExpenseListUseCase _getExpenseListUseCase;
 
   ExpenseListBloc(this._getExpenseListUseCase)
     : super(LoadingExpenseListState()) {
-    on<GetExpenseListEvent>(_getExpenseList);
+    on<GetExpenseListEvent>((event, emit) => _getExpenseList(event, emit));
+
     on<ClickExpenseItemEvent>((event, emit) {
       // Logic of clicking on an item
     });
