433fdceaf358bec312ec68acd1141e2820a1352b
Marsel <mambetaliev.marsel@icloud.com>
2026-04-02 15:45:40 +0600

    change and added colors

--- lib/core/themes/colors.dart
+++ lib/core/themes/colors.dart
@@ -1,21 +1,28 @@
 import 'dart:ui';
 
 // Background Colors
-final Color backgroundColor = Color(0xFFffffff);
+final Color backgroundColor = Color(0xFFF1FFF3);
+final Color appBarColor = Color(0xFF00D09E);
+final Color bottomNavColor = Color(0xFFDFF7E2);
+final Color surfaceColor = Color(0xFFDFF7E2);
 final Color backgroundGreen = Color(0xff4c884c);
 final Color backgroundRed = Color(0xFFfc3f3f);
 
 // Text Colors
-final Color textColorPrimary = Color(0xFF000000);
+final Color textColorPrimary = Color(0xFF052224);
 final Color selectedTextColor = Color(0xFF000000);
 final Color unselectedTextColor = Color(0xFF89888a);
 final Color secondaryTextColor = Color(0xFF666669);
+final Color buttonTextColor = Color(0xFF093030);
 
 // Widget Colors
 final Color widgetColorPrimary = Color(0xFF5856D6);
 final Color widgetColorSecondary = Color(0xfff5f5f5);
 final Color selectedColorPrimary = Color(0xFFc2c3c4);
-final Color primaryButtonColor = Color(0xffb5b4b4);
+final Color primaryButtonColor = Color(0xFF00D09E);
 
 // Stroke Colors
 final Color strokeColorPrimary = Color(0xFFe1e2e3);
+
+// Icon Colors
+final Color selectedBottomNavItemColor = Color(0xFF052224);
