7ddb50ae5eb865291d84f05b1b22a6f972088602
Marsel <mambetaliev.marsel@icloud.com>
2026-04-09 17:53:28 +0600

    Added a check in order to notify the user that there is no income. Changed the widgets for displaying the list

--- lib/features/main/presentation/pages/income_list_screen.dart
+++ lib/features/main/presentation/pages/income_list_screen.dart
@@ -1,3 +1,4 @@
+import 'package:finance_tracker/core/themes/colors.dart';
 import 'package:finance_tracker/core/utils/loading_overlay.dart';
 import 'package:finance_tracker/core/utils/message_snack_bar.dart';
 import 'package:finance_tracker/features/main/presentation/bloc/income/income_list_state.dart';
@@ -5,6 +6,7 @@ import 'package:finance_tracker/features/main/presentation/widgets/movement.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../core/themes/dimens.dart';
 import '../bloc/income/income_list_bloc.dart';
 
 class IncomeListScreen extends StatelessWidget {
@@ -16,12 +18,40 @@ class IncomeListScreen extends StatelessWidget {
       builder: (context, state) {
         if (state is SuccessIncomeListState) {
           LoadingOverlay.hide();
-          final incomeList = state.incomeList;
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Expanded(
-                child: ListView.builder(
+          if (state.incomeList.isEmpty) {
+            return Align(
+              alignment: Alignment.center,
+              child: Text(
+                'Операций нет',
+                style: TextStyle(
+                  color: textColorPrimary,
+                  fontSize: textSize18,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            );
+          } else {
+            final incomeList = state.incomeList;
+            return CustomScrollView(
+              slivers: [
+                SliverToBoxAdapter(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      SizedBox(height: height12),
+                      Text(
+                        'Недавние операции',
+                        style: TextStyle(
+                          color: textColorPrimary,
+                          fontSize: textSize18,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      SizedBox(height: height12),
+                    ],
+                  ),
+                ),
+                SliverList.builder(
                   itemCount: incomeList.length,
                   itemBuilder: (context, index) {
                     final incomeItem = incomeList[index];
@@ -31,9 +61,9 @@ class IncomeListScreen extends StatelessWidget {
                     );
                   },
                 ),
-              ),
-            ],
-          );
+              ],
+            );
+          }
         }
         return SizedBox();
       },
