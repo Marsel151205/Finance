067f151e63578149626f2d25f353942ba92a6915
Marsel <mambetaliev.marsel@icloud.com>
2026-04-09 17:53:57 +0600

    added new dependencies

--- lib/app/di/injection.dart
+++ lib/app/di/injection.dart
@@ -7,14 +7,22 @@ import 'package:finance_tracker/features/add_entry/domain/use_cases/add_income_u
 import 'package:finance_tracker/features/add_entry/domain/use_cases/get_expense_categories_use_case.dart';
 import 'package:finance_tracker/features/add_entry/domain/use_cases/get_income_sources_use_case.dart';
 import 'package:finance_tracker/features/add_entry/presentation/bloc/income/add_income_bloc.dart';
+import 'package:finance_tracker/features/create_balance/presentation/bloc/create_balance_bloc.dart';
 import 'package:finance_tracker/features/main/data/repositories/expense_list_repository_impl.dart';
 import 'package:finance_tracker/features/main/data/repositories/income_list_repository_impl.dart';
 import 'package:finance_tracker/features/main/domain/repositories/expense_list_repository.dart';
 import 'package:finance_tracker/features/main/domain/repositories/income_list_repository.dart';
 import 'package:finance_tracker/features/main/domain/use_cases/get_expense_list_use_case.dart';
 import 'package:finance_tracker/features/main/domain/use_cases/get_income_list_use_case.dart';
+import 'package:finance_tracker/features/main/domain/use_cases/get_total_expense_use_case.dart';
+import 'package:finance_tracker/features/main/domain/use_cases/get_total_income_use_case.dart';
 import 'package:finance_tracker/features/main/presentation/bloc/expense/expense_list_bloc.dart';
 import 'package:finance_tracker/features/main/presentation/bloc/income/income_list_bloc.dart';
+import 'package:finance_tracker/features/main/presentation/bloc/overview/overview_bloc.dart';
+import 'package:finance_tracker/shared/balance/data/repositories/balance_repository_impl.dart';
+import 'package:finance_tracker/shared/balance/domain/repositories/balance_repository.dart';
+import 'package:finance_tracker/shared/balance/domain/use_cases/add_balance_use_case.dart';
+import 'package:finance_tracker/shared/balance/domain/use_cases/get_balance_use_case.dart';
 import 'package:get_it/get_it.dart';
 
 import '../../features/add_entry/domain/repositories/add_expense_repository.dart';
@@ -59,6 +67,9 @@ void initDependencies() {
   serviceLocator.registerLazySingleton<GetExpenseListUseCase>(
     () => GetExpenseListUseCase(serviceLocator<ExpenseListRepository>()),
   );
+  serviceLocator.registerLazySingleton<GetTotalExpenseUseCase>(
+    () => GetTotalExpenseUseCase(serviceLocator()),
+  );
   serviceLocator.registerFactory<ExpenseListBloc>(
     () => ExpenseListBloc(serviceLocator<GetExpenseListUseCase>()),
   );
@@ -70,7 +81,29 @@ void initDependencies() {
   serviceLocator.registerLazySingleton<GetIncomeListUseCase>(
     () => GetIncomeListUseCase(serviceLocator()),
   );
+  serviceLocator.registerLazySingleton<GetTotalIncomeUseCase>(
+    () => GetTotalIncomeUseCase(serviceLocator()),
+  );
   serviceLocator.registerFactory<IncomeListBloc>(
     () => IncomeListBloc(serviceLocator()),
+  );
+
+  // Balance
+  serviceLocator.registerLazySingleton<BalanceRepository>(
+    () => BalanceRepositoryImpl(serviceLocator()),
+  );
+  serviceLocator.registerLazySingleton<GetBalanceUseCase>(
+    () => GetBalanceUseCase(serviceLocator()),
+  );
+  serviceLocator.registerLazySingleton<AddBalanceUseCase>(
+    () => AddBalanceUseCase(serviceLocator()),
+  );
+  serviceLocator.registerFactory<CreateBalanceBloc>(
+    () => CreateBalanceBloc(serviceLocator()),
+  );
+
+  // Overview
+  serviceLocator.registerFactory<OverviewBloc>(
+    () => OverviewBloc(serviceLocator(), serviceLocator(), serviceLocator()),
   );
 }
