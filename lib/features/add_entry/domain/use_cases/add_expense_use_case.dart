4b4540fcfe60c8439b35805799c70158e26cde11
Marsel <mambetaliev.marsel@icloud.com>
2026-04-10 14:44:30 +0600

    implemented expense deduction from the user's balance

--- lib/features/add_entry/domain/use_cases/add_expense_use_case.dart
+++ lib/features/add_entry/domain/use_cases/add_expense_use_case.dart
@@ -1,14 +1,25 @@
 import 'package:finance_tracker/features/add_entry/domain/repositories/add_expense_repository.dart';
+import 'package:finance_tracker/shared/balance/domain/repositories/balance_repository.dart';
 import 'package:fpdart/fpdart.dart';
 
 import '../../../../shared/income_and_expense/domain/entities/expense_entity.dart';
 
 class AddExpenseUseCase {
-  final AddExpenseRepository repository;
+  final AddExpenseRepository _addExpenseRepository;
+  final BalanceRepository _balanceRepository;
 
-  AddExpenseUseCase(this.repository);
+  AddExpenseUseCase(this._addExpenseRepository, this._balanceRepository);
 
   Future<Either<String, Unit>> call(ExpenseEntity model) async {
-    return repository.add(model);
+    final addExpenseResult = await _addExpenseRepository.add(model);
+    final balanceResult = await _balanceRepository.getBalance();
+    return balanceResult.fold((failure) => Left(failure), (balance) {
+      return addExpenseResult.fold((failure) => Left(failure), (
+        isSavedExpense,
+      ) {
+        _balanceRepository.saveBalance(balance.sum - model.sum);
+        return Right(unit);
+      });
+    });
   }
 }
