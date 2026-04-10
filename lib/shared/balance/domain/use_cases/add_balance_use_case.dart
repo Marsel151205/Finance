8823fada3001dd0374d8aac0bfe1a65160a8a5db
Marsel <mambetaliev.marsel@icloud.com>
2026-04-10 14:43:11 +0600

    rescheduled saving a variable to determine if a wallet has been created

--- lib/shared/balance/domain/use_cases/add_balance_use_case.dart
+++ lib/shared/balance/domain/use_cases/add_balance_use_case.dart
@@ -1,14 +1,22 @@
 import 'package:finance_tracker/shared/balance/domain/repositories/balance_repository.dart';
 import 'package:fpdart/fpdart.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AddBalanceUseCase {
   final BalanceRepository _repository;
 
   const AddBalanceUseCase(this._repository);
 
-  Future<Either<String, Unit>> addBalance(int? sum) {
+  Future<Either<String, Unit>> addBalance(int? sum) async {
+    final preferences = await SharedPreferences.getInstance();
     if (sum != null) {
-      return _repository.saveBalance(sum);
+      final saveBalanceResult = await _repository.saveBalance(sum);
+      return saveBalanceResult.fold((failure) => Left(failure), (
+        isSaveBalance,
+      ) {
+        preferences.setBool('isBalanceCreated', true);
+        return Right(unit);
+      });
     } else {
       return Future.value(Left('Введите Баланс'));
     }
